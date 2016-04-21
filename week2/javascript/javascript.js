f1357c09d6aa2dc721b2d72b2454db6b7dce5913
Charles Trillingham <ctrill@uw.edu>
2016-04-21 10:41:44 -0700

    assignment two

---
+++ week2/javascript/javascript.js
@@ -0,0 +1,140 @@
+
+var button = document.querySelector("button");
+button.addEventListener("click", function(){
+  console.log("button clicked.");
+});
+
+//============================================================
+
+
+//create an array
+var dataList = new Array;
+
+//create an array dynamically
+var output = "";
+//this for loop builds the array
+for (var i = 0; i < 25; i++ ){
+	dataList[i] = i + 1;
+}
+
+//this for loop displays the data and checks for null values
+for (var i = 0; i < dataList.length; i++ ){
+	//check for null value first
+	if(dataList[i] != null){
+		output += '<td + dataList[i] />';
+	}
+}
+
+
+function AddData() {
+    var x = document.getElementById("l-name-label").value;
+    var y = document.getElementById("f-name-label").value;
+    var letters = '/^[a-zA-Z]+$/';
+    if ((parseInt(x) != (x)) && (y == parseInt(y))) {
+        alert("Wrong Value Entered");
+    } else {
+        var rows = "";
+        var name = document.getElementById("f-name-label").value;
+        var lastName = document.getElementById("l-name-label").value;
+        var email = document.getElementById("email").value;
+
+        rows += "<tr><td>" + name + "</td><td>" + lastName + "</td><td>" + email + "</td></tr>";
+        var tbody = document.querySelector("#list tbody");
+        var tr = document.createElement("tr");
+      
+        tr.innerHTML = rows;
+        tbody.appendChild(tr)
+        
+    }
+  }
+//
+  function ResetForm(){
+    document.getElementById("basic").reset();
+  }
+
+//
+//
+////partial courtesy to stackoverflow user Qantas 94 Heavy
+//
+//function displayTable(alpha) {
+//  // get the table to add rows to
+//  var table = document.getElementById('alpha');
+//
+//  // cycle through the array for each of the presidents
+//  for (var i = 0; i < dataList.length; ++i) {
+//    // keep a reference to an individual president object
+//    var alpha = alpha[i];
+//
+//    // create a row element to append cells to
+//    var row = document.createElement('tr');
+//
+//    // properties of the array elements
+//    var properties = ['First Name', 'Last Name', 'E-mail Contact'];
+//
+//    // append each one of them to the row in question, in order
+//    for (var j = 0; j < properties.length; ++j) {
+//      // create new data cell for names
+//      var cell = document.createElement('td');
+//
+//      // set name of property using bracket notation (properties[j] is a string,
+//      // which can be used to access properties of president)
+//      cell.innerHTML = alpha[properties[j]];
+//
+//      // add to end of the row
+//      row.appendChild(cell);
+//    }
+//
+//    // add new row to table
+//    table.appendChild(row);
+//  }
+//}
+//
+
+
+//
+////create the buildTable function
+//function buildTable()
+//{
+//	var alphaDiv = document.getElementById("alpha");
+//	
+//	//create an output variable
+//	var output = "";
+//	output += '';
+//		
+//	
+//	for(i in dataList)
+//	{
+//		//concantenation assignment operator..
+//		output += '<tr + '" <td>/tr>';
+//		output += "< >"; 
+//		output += "< >";
+//		output += '</>';
+//		output += '< >'; 
+//	}
+//	
+//	
+//	output += '</div>';
+//	output += '<div class="thumbs_block">  ';
+//	for(var j=0; j < imageListB.length; j++)
+//	{
+//		//concatenation assignment operator..
+//		output += '<img src="' + imageListB[j] + '" />';
+//	}
+//	output += '</div>';
+//	output += '<div class="thumbs_block">  ';
+//// 	
+//	for(var k=0; k < imageListC.length; k++)
+//	{
+//		//concatenation assignment operator..
+//		output += '<>';
+//	}
+//	output += '</div>';
+//	
+//	alphaDiv.innerHTML = output;
+//}
+//
+//window.onload = function()
+//{
+//	buildTable();//use or "fire off" this function
+//	//or invoke the function
+//};
