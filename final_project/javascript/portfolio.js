4280656a9c0c2312893b4dfc2a73e793f8d80ccb
Charles Trillingham <ctrill@uw.edu>
2016-06-07 21:18:11 -0700

    clearer backgrounds for testing

--- final_project/javascript/portfolio.js
+++ final_project/javascript/portfolio.js
@@ -2,5 +2,4 @@ $('.top').on('click', function() {
 	$parent_box = $(this).closest('.portfolio');
 	$parent_box.siblings().find('.bottom').hide();
 	$parent_box.find('.bottom').toggle();
-    event.preventDefault() 
 });
